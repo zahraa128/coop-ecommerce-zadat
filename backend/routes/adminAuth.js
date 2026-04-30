11cf31643102087854d792232e191daf74c710cc
zahraa lakkis <zlakkis4@gmail.com>
2026-04-30 15:44:20 +0300

    add jwt authentication system

--- backend/routes/adminAuth.js
+++ backend/routes/adminAuth.js
@@ -1,32 +1,39 @@
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const supabase = require("../supabase");
-
 
 // POST /api/admin/auth/login
 app.post("/api/login", async (req, res) => {
   try {
     const { username, password } = req.body;
 
-    
-const { data, error } = await supabase
-  .from("admins")
-  .select("*")
-  .eq("username", username)
-  .single();
+    const { data, error } = await supabase
+      .from("admins")
+      .select("*")
+      .eq("username", username)
+      .single();
 
-if (error || !data) {
-  return res.status(401).json({ message: "Invalid username" });
-}
+    if (error || !data) {
+      return res.status(401).json({ message: "Invalid username" });
+    }
 
-const match = await bcrypt.compare(password, data.password);
+    const match = await bcrypt.compare(password, data.password);
 
-if (!match) {
-  return res.status(401).json({ message: "Invalid password" });
-}
+    if (!match) {
+      return res.status(401).json({ message: "Invalid password" });
+    }
+
+    const token = jwt.sign(
+      { id: data.id, username: data.username },
+      "SECRET_KEY_123",
+      { expiresIn: "1d" }
+    );
+
     res.json({
       success: true,
+      token,
       user: {
         id: data.id,
         username: data.username
@@ -38,5 +45,4 @@ if (!match) {
     res.status(500).json({ message: "Server error" });
   }
 });
-
 module.exports = router;
