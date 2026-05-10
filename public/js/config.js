6773abe0616c74ebe8b0b15d18083e08edb9d381
zahraa lakkis <zlakkis4@gmail.com>
2026-05-10 08:33:42 +0300

    add vercel speed insights

--- public/js/config.js
+++ public/js/config.js
@@ -1,1 +1,21 @@
 const API_URL = "https://coop-backend-hecq.onrender.com";
+
+(() => {
+  if (document.head.querySelector('script[data-vercel-speed-insights="true"]')) {
+    return;
+  }
+
+  window.si = window.si || function (...params) {
+    window.siq = window.siq || [];
+    window.siq.push(params);
+  };
+
+  const script = document.createElement("script");
+  script.defer = true;
+  script.src = "/_vercel/speed-insights/script.js";
+  script.dataset.sdkn = "@vercel/speed-insights";
+  script.dataset.sdkv = "2.0.0";
+  script.dataset.vercelSpeedInsights = "true";
+
+  document.head.appendChild(script);
+})();
